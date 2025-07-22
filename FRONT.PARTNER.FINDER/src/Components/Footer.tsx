7bbf46b3fb8171ba546d0ef97b0245c7d66ebae0
Evaldo Rodrigues <110418142+evaaldo@users.noreply.github.com>
2025-07-21 21:13:30 -0300

    Delete FRONT.PARTNER.FINDER directory

--- FRONT.PARTNER.FINDER/src/Components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export function Footer() {
-    return (
-        <div className="bg-blue-300 w-full text-center h-[7vh] flex justify-center items-center">
-            <a href="https://github.com/evaaldo" className="text-white underline">Conheça mais sobre o desenvolvedor desse sistema clicando aqui</a>
-        </div>
-    )
-}
--- FRONT.PARTNER.FINDER/src/Components/Header.tsx
+++
@@ -1,7 +0,0 @@
-export function Header() {
-    return(
-        <header className="bg-blue-300 flex justify-center items-center py-5 w-full h-[10vh]">
-            <img src="fastliver.png" alt="logo" />
-        </header>
-    );
-}
